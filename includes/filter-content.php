ef699012e34a35ef82f70c152c182377a54ff69d
Marco Di Bella <77158987+mdibella-dev@users.noreply.github.com>
2024-06-25 09:56:47 +0200

    Code style: Replace hard tabs with soft tabs

--- includes/filter-content.php
+++ includes/filter-content.php
@@ -13,11 +13,11 @@ add_filter( 'the_content', 'fx_toc_filter_content' );
  */
 function fx_toc_filter_content( $content ){
 
-	/* Only in singular and shortcode exist in content */
-	if ( !is_admin() && has_shortcode( get_post_field( 'post_content', get_the_ID() ), 'toc' ) ) {
-		$content = fx_toc_add_span_to_headings( $content );
-	}
-	return $content;
+    /* Only in singular and shortcode exist in content */
+    if ( !is_admin() && has_shortcode( get_post_field( 'post_content', get_the_ID() ), 'toc' ) ) {
+        $content = fx_toc_add_span_to_headings( $content );
+    }
+    return $content;
 }
 
 /**
@@ -25,9 +25,9 @@ function fx_toc_filter_content( $content ){
  * @since 0.1.0
  */
 function fx_toc_add_span_to_headings( $content ){
-	fx_toc_sc_unique_names_reset(); // reset num
-	$content = preg_replace_callback( "#<h([1-6]).*?>(.*?)</h[1-6]>#i", "fx_toc_heading_anchor", $content );
-	return $content;
+    fx_toc_sc_unique_names_reset(); // reset num
+    $content = preg_replace_callback( "#<h([1-6]).*?>(.*?)</h[1-6]>#i", "fx_toc_heading_anchor", $content );
+    return $content;
 }
 
 /**
@@ -35,10 +35,10 @@ function fx_toc_add_span_to_headings( $content ){
  * @since 0.1.0
  */
 function fx_toc_heading_anchor( $match ) {
-	$name = fx_toc_sc_get_unique_name( $match[2] );
-	$heading = absint( $match[1] );
-	$text = $match[2];
-	return "<h{$heading}>" . '<span id="' . sanitize_title( $name ) . '">' . $text . '</span>' . "</h{$heading}>";
+    $name = fx_toc_sc_get_unique_name( $match[2] );
+    $heading = absint( $match[1] );
+    $text = $match[2];
+    return "<h{$heading}>" . '<span id="' . sanitize_title( $name ) . '">' . $text . '</span>' . "</h{$heading}>";
 }
 
 
