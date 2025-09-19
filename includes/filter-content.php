2cd1e7dc0e52095d78deb0325d99af707b7b5e0d
Marco Di Bella <77158987+mdibella-dev@users.noreply.github.com>
2025-09-19 11:19:11 +0200

    Update filter-content.php

--- includes/filter-content.php
+++ includes/filter-content.php
@@ -21,7 +21,7 @@ add_filter( 'the_content', 'fx_toc_filter_content' );
 
 function fx_toc_filter_content( $content ) {
 
-/* Only in singular and shortcode exist in content */
+    /* Only in singular and shortcode exist in content */
     if ( ! is_admin() and has_shortcode( get_post_field( 'post_content', get_the_ID() ), 'toc' ) ) {
         $content = fx_toc_add_span_to_headings( $content );
     }
