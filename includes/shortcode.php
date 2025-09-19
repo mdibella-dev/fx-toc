a91a3a4cb912186500c2027bf06fa2c69953bb28
Marco Di Bella <77158987+mdibella-dev@users.noreply.github.com>
2025-09-19 10:09:04 +0200

    Revert "Remove unused array var"
    This reverts commit 40db3d2e40b287c0d18386a468b2b37410f42b2c.

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -22,6 +22,9 @@ function fx_toc_shortcode( $atts ) {
     /* Get globals */
     global $post;
 
+    /* Reset used names (?) */
+    $fx_toc_used_names = array();
+
     /* Default shortcode attr */
     $default_args = apply_filters( 'fx_toc_default_args', [
         'depth'          => 6,
